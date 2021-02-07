13c21f5c9da3698bc58f4889a2781374bb937f97
OscarFlores-IFi <41029452+OscarFlores-IFi@users.noreply.github.com>
2021-02-06 19:10:32 -0600

    2 Different simulations
    Different information is provided in each simulation. 
    The .xlsx file assumes that every produced unit should be used, otherwise it had no value at all. 
    The .py simulation assumes that every produced unit (even though it is not required) has some market value, so it may be added to the revenues. 
    
    Both simulations suggest that in this business model, whenever it is possible to ask for money and invest it in building upgrades, it may be done.

---
+++ simcompanies.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+class Resources(object):
+    
+    cash = 0 
+    water = 0
+    seeds = 0 
+    grain = 0
+    sausages = 0
+
+    @staticmethod
+    def info():
+        # resources hourly balance
+        print('Cash: {}'.format(Resources.cash))
+        print('Water: {}'.format(Resources.water))
+        print('Seeds: {}'.format(Resources.seeds)) 
+        print('Grain: {}'.format(Resources.grain))
+        print('Sausages: {}'.format(Resources.sausages))
+    
+    @staticmethod
+    def info_daily():
+        # resources daily balance
+        print('Cash: {}'.format(Resources.cash * 24))
+        print('Water: {}'.format(Resources.water * 24))
+        print('Seeds: {}'.format(Resources.seeds * 24)) 
+        print('Grain: {}'.format(Resources.grain * 24))
+        print('Sausages: {}'.format(Resources.sausages * 24))
+        
+    @staticmethod
+    def revenue():
+        water_price = 0.295
+        seeds_price = 0.18
+        grain_price = 0.49
+        sausages_price = 3.7
+        rev =   Resources.cash + \
+            water_price*Resources.water + \
+            seeds_price*Resources.seeds + \
+            grain_price*Resources.grain + \
+            sausages_price*Resources.sausages
+        return(rev)
+
+    @staticmethod
+    def revenue_daily():
+        return (Resources.revenue() * 24)
+        
+        
+class Plantation(object):
+    def __init__(self, lvl):
+        self.lvl = lvl
+        self.grain_production = 825 * lvl
+        self.grain_input_resources = [0.5, 1] # [water, seeds]
+        self.wages = 104 * admin_overhead * lvl
+        self.upgrading_cost = 6900 * lvl 
+        
+        Resources.cash -= self.wages
+        Resources.water -= self.grain_input_resources[0] * self.grain_production
+        Resources.seeds -= self.grain_input_resources[1] * self.grain_production
+        Resources.grain += self.grain_production
+    
+    def info(self):
+        print('Plantation lvl: ' + str(self.lvl))
+        print('Grain production: ' + str(self.grain_production))
+        print('Grain input resources per hour: {} water, {} seeds '.format(
+            self.grain_input_resources[0] * self.grain_production,
+            self.grain_input_resources[1] * self.grain_production))
+        print('Wages: ' + str(self.wages))
+        print('Upgrading Cost: ' + str(self.upgrading_cost))        
+
+
+class Farm(object):
+    def __init__(self, lvl):
+        self.lvl = lvl
+        self.sausage_production = 138.4 * lvl
+        self.sausage_input_resources = [3, 2] # [water, grain]
+        self.wages = 138  * admin_overhead * lvl
+        self.upgrading_cost = 10350 * lvl 
+        
+        Resources.cash -= self.wages
+        Resources.water -= self.sausage_input_resources[0] * self.sausage_production
+        Resources.grain -= self.sausage_input_resources[1] * self.sausage_production
+        Resources.sausages += self.sausage_production
+
+    def info(self):
+        print('Farm lvl: ' + str(self.lvl))
+        print('Sausage_production: ' + str(self.sausage_production))
+        print('Sausage input resources per hour: {} water, {} grain '.format(
+            self.sausage_input_resources[0] * self.sausage_production,
+            self.sausage_input_resources[1] * self.sausage_production))        
+        print('Wages: ' + str(self.wages))
+        print('Upgrading Cost: ' + str(self.upgrading_cost))
+        
+
+class Grocery(object):
+    def __init__(self, lvl, retail_price, units_sold_per_hour):
+        self.sausage_retail_price = retail_price 
+        self.lvl = lvl
+        self.sales = units_sold_per_hour * lvl
+        self.wages = 138 * admin_overhead * lvl
+        self.upgrading_cost = 10350 * lvl 
+
+        Resources.cash += self.sales * self.sausage_retail_price - self.wages
+        Resources.sausages -= self.sales
+        
+        
+    def info(self):
+        print('Grocery lvl: ' + str(self.lvl))
+        print('Sold sausages per hour: ' + str(self.sales))
+        print('Wages: ' + str(self.wages))
+        print('Sales - wages, per hour: ' + str(self.sales * self.sausage_retail_price - self.wages))
+        print('Upgrading Cost: ' + str(self.upgrading_cost))
+
+
+##### 
+##### Simulations!
+#####
+
+admin_overhead = 1.13
+        
+P1 = Plantation(3)
+# P1.info()
+
+F1 = Farm(5)
+F2 = Farm(3)
+# F1.info()
+
+rp, usph = 6.1, 80 
+
+G1 = Grocery(3, rp, usph) # lvl, retail price, units sold per hour (at lvl 1)
+G2 = Grocery(3, rp, usph)
+G3 = Grocery(3 ,rp, usph)
+G4 = Grocery(3, rp, usph)
+# G1.info()
+
+
+Resources.info_daily()
+
+print('Total revenue: ' + str(Resources.revenue_daily()))
